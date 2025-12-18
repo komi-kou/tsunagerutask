080a2e5bb6839a68a953ca873e8512ff809221b1
komi-kou <komiya11122@gmail.com>
2025-12-18 15:17:57 +0900

    Add files via upload

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCY7gHYGzhgFPBQm99syARDbnflMJbloSc",
+    authDomain: "task-c9560.firebaseapp.com",
+    projectId: "task-c9560",
+    storageBucket: "task-c9560.firebasestorage.app",
+    messagingSenderId: "719368236286",
+    appId: "1:719368236286:web:04f765f5429def1c59c9af",
+    measurementId: "G-WN2J3K527D"
 };
 
 // Initialize Firebase
